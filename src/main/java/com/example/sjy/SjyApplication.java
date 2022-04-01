2c04756f5ac6ba7c74610b5c5c6c6049ba1c4372
shijiyong <1771847160@qq.com>
2022-04-01 11:25:51 +0800

    加入项目

---
+++ src/main/java/com/example/sjy/SjyApplication.java
@@ -0,0 +1,13 @@
+package com.example.sjy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SjyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sjy/SjyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sjy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
